66d50d83402190051a9c09c8023e6c5c9f9ac1ca
AnnaGP <AnnaGP@users.noreply.github.com>
2025-02-27 16:28:48 +0000

    Removing example models

---
+++ coffee_shop_tammikuu/models/customer.sql
@@ -0,0 +1,19 @@
+with orders_per_customer as (
+    select 
+        customer_id,
+        min(created_at) as first_order_at, 
+        count(id) as number_of_orders 
+    from `analytics-engineers-club.coffee_shop.orders`
+    group by 1
+)
+
+select
+    customer_id,     
+    customers.name,
+    customers.email,
+    coalesce(orders_per_customer.number_of_orders, 0) as n_orders,
+    orders_per_customer.number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` customers
+left join orders_per_customer on customers.id = orders_per_customer.customer_id
+order by orders_per_customer.first_order_at 
+
